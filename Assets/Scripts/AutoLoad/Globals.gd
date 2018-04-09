299255360a035436811c72b9ca9ad71c6b6a4691
Sam Wedgwood <sjwedgwood@gmail.com>
2018-04-09 20:34:02 +0100

    Added locks to doors

--- Assets/Scripts/AutoLoad/Globals.gd
+++ Assets/Scripts/AutoLoad/Globals.gd
@@ -2,6 +2,14 @@ extends Node
 
 onready var game_size = OS.get_window_size()
 var player_pos = Vector2(0, 0)
+var red_door = false
+var blue_door = false
+var green_door = false
+var yellow_door = false
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		red_door = true
 
 func change_room(scene):
 	get_tree().change_scene("res://Scenes/"+scene+".tscn")
--- Assets/Scripts/Props/RedDoor.gd
+++ Assets/Scripts/Props/RedDoor.gd
@@ -1,7 +1,17 @@
 extends Area2D
 
 onready var player = get_node("../../Ashley")
+var locked = true
+
+func _ready():
+	if Globals.red_door:
+		locked = false
+		get_node("Lock").hide()
 
 func _input(event):
-	if event.is_action_pressed("use") and overlaps_body(player):
-		Globals.change_room("RedRoom")
+	if event.is_action_pressed("use") and overlaps_body(player) and Globals.red_door:
+		if locked:
+			get_node("Lock").hide()
+			locked = false
+		else:
+			Globals.change_room("RedRoom")
